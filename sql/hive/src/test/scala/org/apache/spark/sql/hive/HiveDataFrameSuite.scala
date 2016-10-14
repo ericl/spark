a7c0d35ec98d68024e62725c3915b811d7a29e93
Eric Liang <ekl@databricks.com>
2016-10-13 18:09:55 -0700

    Thu Oct 13 18:09:55 PDT 2016

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveDataFrameSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveDataFrameSuite.scala
@@ -77,7 +77,7 @@ class HiveDataFrameSuite extends QueryTest with TestHiveSingleton with SQLTestUt
     }
   }
 
-  test("metastore partition pruning reads only necessary partition data") {
+  test("lazy partition pruning reads only necessary partition data") {
     withSQLConf("spark.sql.hive.filesourcePartitionPruning" -> "true") {
       withTable("test") {
         withTempDir { dir =>
